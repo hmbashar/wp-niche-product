43c98bfec0d45430002b0754d9a86d41a1234145
hmbashar <hmbashar@gmail.com>
2019-02-04 07:59:30 +0600

    column fix

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -4,41 +4,53 @@ function wp_niche_product_shortcode($wp_niche){
 	ob_start();
 	extract(shortcode_atts(array( 
 	'link'			=> '',
-    'cb_name'		=> '',
-    'cb_title'		=>'',
+    'wnp_name'		=> '',
+    'wnp_title'		=> '',
     'img_url'		=> '',
-    'link_content'	=>'',
-    'star_icon'		=>'',
+    'link_text'		=> '',
+    'rating'		=> 5,
+    'icon'			=> 'fas fa-star',
+    'column'		=> 4,
 	), $wp_niche));
 ?>
 
                <!--singal section one -->
-                    <div class="col-xl-4 col-md-6 col-12 p-0">
+                    <div class="col-xl-<?php echo esc_attr($column);?> col-md-6 col-12">
                         <div class="singal_cb_table_column_package"> 
                             <div class="cb_table_column_package"> 
                                 <div class="cb_table_column_singal_package"> </div>
                                 <div class="cb_table_column_package_name"> 
-                                    <h3><?php echo esc_html($cb_name); ?></h3>
-                                    <i class="fas fa-star"></i>
+                                	<?php if(!empty($wnp_name)) : ?>
+                                    	<h3><?php echo esc_html($wnp_name); ?></h3>
+                                    <?php endif; ?>
+                                    <?php if(!empty($icon)) : ?>
+                                    	<i class="<?php echo esc_attr($icon); ?>"></i>
+                                    <?php endif; ?>
                                 </div>
                             </div>
 	                        <div class="cb_table_column_product"> 
 	                            <div class="cb_table_column_product_img"> 
-	                                <img src="<?php echo esc_url($img_url)?>" alt="">
+	                            	<?php if(!empty($img_url)) : ?>
+	                                	<img src="<?php echo esc_url($img_url)?>" alt="">
+	                                <?php endif; ?>
 	                            </div>
 	                            <div class="cb_table_column_product_title"> 
-	                                <h2><a href="<?php echo esc_url($link);?>"><?php echo esc_html($cb_title); ?></a></h2>
+	                            	<?php if(!empty($wnp_title)) : ?>
+	                                	<h2><a href="<?php echo esc_url($link);?>"><?php echo esc_html($wnp_title); ?></a></h2>
+	                                <?php endif; ?>
 	                            </div>
 	                            <div class="cb_table_column_product_review_icon">						
-									<?php for ($star_icon = 0; $star_icon <= 4; $star_icon++) {
-										  echo '<i class="fas fa-star"></i>';
-									}  ?>
+									<?php for ($feed_star = 0; $feed_star < $rating; $feed_star++) : ?>
+										  <i class="fas fa-star"></i>
+									<?php endfor;  ?>
 	                             </div>
 	                        </div>
 	                        <div class="cb_table_column_product_check_price"> 
 	                            <div class="cb_table_column_product_button"> 
 	                                <div class="cb_table_column_product_button_name"> 
-	                                    <h4><a href="<?php echo esc_url($link);?>"><?php echo esc_html($link_content);?><i class="fas fa-arrow-right"></i></a></h4>
+	                                	<?php if(!empty($link_text)) : ?>
+	                                    	<h4><a href="<?php echo esc_url($link);?>"><?php echo esc_html($link_text);?> <i class="fas fa-arrow-right"></i></a></h4>
+	                                    <?php endif; ?>
 	                                </div>
 	                            </div>
 	                        </div>
@@ -49,4 +61,31 @@ function wp_niche_product_shortcode($wp_niche){
 	return ob_get_clean();
 	}
 add_shortcode( 'wp_niche_product', 'wp_niche_product_shortcode');
+		
+
+function wp_niche_products_row($atts, $content = null){ 
+	ob_start();
+?>
+
+		<section class="cb_table_column_area clearfix"> 
+		    <div class="cb_table_column">	
+	
+		    		
+		    	        
+	            <div class="cb_table_area">
+	               <div class="row">
+	        			<?php if(!empty($content)) {
+	        					echo do_shortcode( $content );
+	        				}
+	        			?>
+	                </div>               
+	            </div> 	
+
+		    </div>
+		</section>
+
+	<?php
+	return ob_get_clean();
+	}
+add_shortcode( 'wp_niche_row', 'wp_niche_products_row');
 		
